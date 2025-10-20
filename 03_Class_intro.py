e6c267a149092142ebfec495271f36d3c6f1d1e8
Brad Simpson <bradsimpson@icloud.com>
2025-10-20 12:57:50 -0400

    added instance methods lecture content

--- 03_Class_intro.py
+++ 03_Class_intro.py
@@ -1,4 +1,5 @@
 # Class Syntax!
+from random import choice
 
 class Cat:
     def __init__(self, color, age, name="Kitty"):
@@ -8,7 +9,19 @@ class Cat:
         self.toys = ["ball", "toy mouse", "yarn"]
 
 
+    def speak(self):
+        return f"{self.name} says, 'Meow!'"
+    
+
+    def play_with_toy(self):
+        return f"{self.name} plays with their {choice(self.toys)}"
+    
+
+
 blue = Cat("black", 9, "Blue")
 patch = Cat("tuxedo", 9, "Patch")
 print(blue.name)
 print(patch.age)
+print(blue.speak())
+print(patch.speak())
+print(blue.play_with_toy())
