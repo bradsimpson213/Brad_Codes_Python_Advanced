6854774fdc961585a9cd12cba0922ae1071b01bf
Brad Simpson <bradsimpson@icloud.com>
2025-10-24 10:50:32 -0400

    updated for classmethod lecture

--- 03_Class_intro.py
+++ 03_Class_intro.py
@@ -1,6 +1,7 @@
 # Class Syntax!
 from random import choice
 
+
 class Cat:
     def __init__(self, color, age, name="Kitty"):
         self.color = color
@@ -17,9 +18,18 @@ class Cat:
         return f"{self.name} plays with their {choice(self.toys)}"
     
 
+    @classmethod 
+    def cat_factory(cls, cats):    
+        new_cats = [cls(color, age, name) for color, age, name in cats]
+        print(new_cats)
+        print([cat.speak() for cat in new_cats])
+        return new_cats
 
-blue = Cat("black", 9, "Blue")
-patch = Cat("tuxedo", 9, "Patch")
+
+many_cats = Cat.cat_factory([("black", 9, "Blue"), ("tuxedo", 9, "Patch"),("gray", 15, "Fifi")])
+blue, patch, fifi = many_cats
+# blue = Cat("black", 9, "Blue")
+# patch = Cat("tuxedo", 9, "Patch")
 print(blue.name)
 print(patch.age)
 print(blue.speak())
