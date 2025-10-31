aea4668be12bfc5fa7489f6089b47e726823152a
Brad Simpson <bradsimpson@icloud.com>
2025-10-31 12:13:55 -0400

    added static method to cat class

--- 03_Class_intro.py
+++ 03_Class_intro.py
@@ -24,14 +24,25 @@ class Cat:
         print(new_cats)
         print([cat.speak() for cat in new_cats])
         return new_cats
+    
+
+    @staticmethod 
+    def human_to_cat_years(human_age):
+        if human_age <= 2:
+            return human_age * 12.5
+        else:
+            return 25 + (human_age - 2) * 4
+        
 
 
 many_cats = Cat.cat_factory([("black", 9, "Blue"), ("tuxedo", 9, "Patch"),("gray", 15, "Fifi")])
 blue, patch, fifi = many_cats
 # blue = Cat("black", 9, "Blue")
 # patch = Cat("tuxedo", 9, "Patch")
-print(blue.name)
-print(patch.age)
-print(blue.speak())
-print(patch.speak())
-print(blue.play_with_toy())
+# print(blue.name)
+# print(patch.age)
+# print(blue.speak())
+# print(patch.speak())
+# print(blue.play_with_toy())
+print(Cat.human_to_cat_years(5))
+print(blue.human_to_cat_years(blue.age))
